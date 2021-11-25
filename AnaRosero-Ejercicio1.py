43fc648f07b0141c79dc768ba7de33cb7860212a
ACRosero <95010217+ACRosero@users.noreply.github.com>
2021-11-24 23:39:24 -0500

    Add files via upload

---
+++ AnaRosero-Ejercicio1.py
@@ -0,0 +1,33 @@
+#EJERCICIO 1
+#PARTE 1
+print('\nEJERCICIO 1\n')
+print('\nPARTE 1\n')
+print('\nEmpezando a trabajar con Python')
+print('Realizado por: Ana Rosero\n')
+print('\nEl tipo de dato de 875 es:\n',type(875))
+print('\nEl tipo de dato de 4.89 es:\n',type(4.89))
+print('\nEl tipo de dato del texto: Now is better than never es\n',type('Now is better than never es'))
+print('\n¿El valor 5 + 8i corresponde a un valor entero?\n',type(5+8))
+print('\n¿El valor 8.2 corresponde a un valor entero?\n',type(8.2))
+print('\n¿El texto: Readability counts. corresponde a un string?\n',type('Readability counts.'))
+
+
+#PARTE 2
+#Programa que identifica el tipo de dato de un valor ingresado por el usuario, se realizarán cinco interacciones:
+print('\nPARTE 2\n')
+variable1=input('Primera Interacción, ingrese un valor cualquiera: ') 
+print('Este tipo de dato en Python es:\n',type(variable1))
+
+variable2=input('Segunda Interacción, ingrese un valor cualquiera: ') 
+print('Este tipo de dato en Python es:\n',type(variable2))
+
+variable3=input('Tercera Interacción, ingrese un valor cualquiera: ') 
+print('Este tipo de dato en Python es:\n',type(variable3))
+
+variable4=input('Cuarta Interacción, ingrese un valor cualquiera: ') 
+print('Este tipo de dato en Python es:\n',type(variable4))
+
+variable5=input('Quinta Interacción, ingrese un valor cualquiera: ') 
+print('Este tipo de dato en Python es:\n',type(variable5))
+
+print('\n¡YA NO SE HARÁN MÁS INTERACCIONES!')
